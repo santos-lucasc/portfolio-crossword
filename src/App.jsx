a4c32b5cd4f2d311cd136622595436af0345fd1e
Lucas Santos <lucas.santos97@acad.espm.br>
2025-02-17 13:33:31 -0300

    Adicionado hover

--- src/App.jsx
+++ src/App.jsx
@@ -5,45 +5,43 @@ function App() {
   return (
     <div className="app">
       <div className="crossword">
-        {/* LUCAS (horizontal) */}
-        <div className="cell" style={{ gridRow: 3, gridColumn: 1 }}>
+        {/* LUCAS CORREA (horizontal) */}
+        <div className="cell lucascorrea" style={{ gridRow: 3, gridColumn: 1 }}>
           <span className="number">1</span>
           L
         </div>
-        <div className="cell" style={{ gridRow: 3, gridColumn: 2 }}>U</div>
-        <div className="cell" style={{ gridRow: 3, gridColumn: 3 }}>C</div>
-        <div className="cell" style={{ gridRow: 3, gridColumn: 4 }}>A</div>
-        <div className="cell" style={{ gridRow: 3, gridColumn: 5 }}>S</div>
-
-        {/* CORREA (horizontal) */}
-        <div className="cell" style={{ gridRow: 3, gridColumn: 6 }}>C</div>
-        <div className="cell" style={{ gridRow: 3, gridColumn: 7 }}>O</div>
-        <div className="cell" style={{ gridRow: 3, gridColumn: 8 }}>R</div>
-        <div className="cell" style={{ gridRow: 3, gridColumn: 9 }}>R</div>
-        <div className="cell" style={{ gridRow: 3, gridColumn: 10 }}>Ê</div>
-        <div className="cell" style={{ gridRow: 3, gridColumn: 11 }}>A</div>
+        <div className="cell lucascorrea" style={{ gridRow: 3, gridColumn: 2 }}>U</div>
+        <div className="cell lucascorrea" style={{ gridRow: 3, gridColumn: 3 }}>C</div>
+        <div className="cell lucascorrea" style={{ gridRow: 3, gridColumn: 4 }}>A</div>
+        <div className="cell lucascorrea" style={{ gridRow: 3, gridColumn: 5 }}>S</div>
+        <div className="cell lucascorrea" style={{ gridRow: 3, gridColumn: 6 }}>C</div>
+        <div className="cell lucascorrea" style={{ gridRow: 3, gridColumn: 7 }}>O</div>
+        <div className="cell lucascorrea" style={{ gridRow: 3, gridColumn: 8 }}>R</div>
+        <div className="cell lucascorrea" style={{ gridRow: 3, gridColumn: 9 }}>R</div>
+        <div className="cell lucascorrea" style={{ gridRow: 3, gridColumn: 10 }}>Ê</div>
+        <div className="cell lucascorrea" style={{ gridRow: 3, gridColumn: 11 }}>A</div>
 
         {/* SOBRE (vertical) */}
-        <div className="cell" style={{ gridRow: 3, gridColumn: 5 }}>
+        <div className="cell sobre" style={{ gridRow: 3, gridColumn: 5 }}>
           <span className="number">2</span>
           S
         </div>
-        <div className="cell" style={{ gridRow: 4, gridColumn: 5 }}>O</div>
-        <div className="cell" style={{ gridRow: 5, gridColumn: 5 }}>B</div>
-        <div className="cell" style={{ gridRow: 6, gridColumn: 5 }}>R</div>
-        <div className="cell" style={{ gridRow: 7, gridColumn: 5 }}>E</div>
+        <div className="cell sobre" style={{ gridRow: 4, gridColumn: 5 }}>O</div>
+        <div className="cell sobre" style={{ gridRow: 5, gridColumn: 5 }}>B</div>
+        <div className="cell sobre" style={{ gridRow: 6, gridColumn: 5 }}>R</div>
+        <div className="cell sobre" style={{ gridRow: 7, gridColumn: 5 }}>E</div>
 
         {/* PROJETO (vertical) */}
-        <div className="cell" style={{ gridRow: 1, gridColumn: 7 }}>
+        <div className="cell projeto" style={{ gridRow: 1, gridColumn: 7 }}>
           <span className="number">3</span>
           P
         </div>
-        <div className="cell" style={{ gridRow: 2, gridColumn: 7 }}>R</div>
-        <div className="cell" style={{ gridRow: 3, gridColumn: 7 }}>O</div>
-        <div className="cell" style={{ gridRow: 4, gridColumn: 7 }}>J</div>
-        <div className="cell" style={{ gridRow: 5, gridColumn: 7 }}>E</div>
-        <div className="cell" style={{ gridRow: 6, gridColumn: 7 }}>T</div>
-        <div className="cell" style={{ gridRow: 7, gridColumn: 7 }}>O</div>
+        <div className="cell projeto" style={{ gridRow: 2, gridColumn: 7 }}>R</div>
+        <div className="cell projeto" style={{ gridRow: 3, gridColumn: 7 }}>O</div>
+        <div className="cell projeto" style={{ gridRow: 4, gridColumn: 7 }}>J</div>
+        <div className="cell projeto" style={{ gridRow: 5, gridColumn: 7 }}>E</div>
+        <div className="cell projeto" style={{ gridRow: 6, gridColumn: 7 }}>T</div>
+        <div className="cell projeto" style={{ gridRow: 7, gridColumn: 7 }}>O</div>
       </div>
     </div>
   )
